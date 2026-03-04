40a08d96fa0adfc8f4759a24b264d79b27708e02
Test Test <test.x1011@gmail.com>
2026-03-04 08:33:22 -0600

    refactor: mejorar la tipificación del método getCommonPinningStyles

--- src/components/table/utils.ts
+++ src/components/table/utils.ts
@@ -1,16 +1,20 @@
 import type { Column } from '@tanstack/react-table';
 
-export const getCommonPinningStyles = (column: Column<any>) => {
+export const getCommonPinningStyles = <TData>(
+  column: Column<TData, unknown>,
+): Record<string, unknown> => {
   const isPinned = column.getIsPinned();
-  const isLastLeftPinnedColumn = isPinned === 'left' && column.getIsLastColumn('left');
-  const isFirstRightPinnedColumn = isPinned === 'right' && column.getIsFirstColumn('right');
+  const isLastLeftPinnedColumn =
+    isPinned === 'left' && column.getIsLastColumn('left');
+  const isFirstRightPinnedColumn =
+    isPinned === 'right' && column.getIsFirstColumn('right');
 
   return {
     boxShadow: isLastLeftPinnedColumn
       ? '-4px 0 4px -4px gray inset'
       : isFirstRightPinnedColumn
-      ? '4px 0 4px -4px gray inset'
-      : undefined,
+        ? '4px 0 4px -4px gray inset'
+        : undefined,
     left: isPinned === 'left' ? `${column.getStart('left')}px` : undefined,
     right: isPinned === 'right' ? `${column.getAfter('right')}px` : undefined,
     opacity: 1,
