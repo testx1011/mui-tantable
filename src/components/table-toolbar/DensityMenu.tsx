848aeaecee0270d04227c857a2b62ea0b0bfe010
Test Test <test.x1011@gmail.com>
2026-03-04 08:33:36 -0600

    refactor: mejorar la legibilidad del componente DensityMenu

--- src/components/table-toolbar/DensityMenu.tsx
+++ src/components/table-toolbar/DensityMenu.tsx
@@ -1,5 +1,6 @@
 import { Menu, MenuItem } from '@mui/material';
 import type { Density } from '../../types/core';
+import { JSX } from 'react';
 
 interface Props {
   anchorEl: HTMLElement | null;
@@ -9,12 +10,42 @@ interface Props {
   onDensityChange?: (d: Density) => void;
 }
 
-export function DensityMenu({ anchorEl, open, onClose, density, onDensityChange }: Props) {
+export function DensityMenu({
+  anchorEl,
+  open,
+  onClose,
+  density,
+  onDensityChange,
+}: Props): JSX.Element {
   return (
     <Menu anchorEl={anchorEl} open={open} onClose={onClose}>
-      <MenuItem onClick={() => { onDensityChange?.('compact'); onClose(); }} selected={density === 'compact'}>Compact</MenuItem>
-      <MenuItem onClick={() => { onDensityChange?.('standard'); onClose(); }} selected={density === 'standard'}>Standard</MenuItem>
-      <MenuItem onClick={() => { onDensityChange?.('comfortable'); onClose(); }} selected={density === 'comfortable'}>Comfortable</MenuItem>
+      <MenuItem
+        onClick={() => {
+          onDensityChange?.('compact');
+          onClose();
+        }}
+        selected={density === 'compact'}
+      >
+        Compact
+      </MenuItem>
+      <MenuItem
+        onClick={() => {
+          onDensityChange?.('standard');
+          onClose();
+        }}
+        selected={density === 'standard'}
+      >
+        Standard
+      </MenuItem>
+      <MenuItem
+        onClick={() => {
+          onDensityChange?.('comfortable');
+          onClose();
+        }}
+        selected={density === 'comfortable'}
+      >
+        Comfortable
+      </MenuItem>
     </Menu>
   );
 }
