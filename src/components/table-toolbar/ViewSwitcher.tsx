306d6dc5ed858df743cc44aa041401c53c6c538d
Test Test <test.x1011@gmail.com>
2026-03-04 08:33:59 -0600

    refactor: mejorar la tipificación y legibilidad del componente ViewSwitcher

--- src/components/table-toolbar/ViewSwitcher.tsx
+++ src/components/table-toolbar/ViewSwitcher.tsx
@@ -1,18 +1,21 @@
 import { ToggleButtonGroup, ToggleButton } from '@mui/material';
 import ViewModuleIcon from '@mui/icons-material/ViewModule';
 import ViewListIcon from '@mui/icons-material/ViewList';
+import { JSX } from 'react';
 
 interface Props {
   view: 'grid' | 'list';
   onViewChange?: (v: 'grid' | 'list') => void;
 }
 
-export function ViewSwitcher({ view, onViewChange }: Props) {
+export function ViewSwitcher({ view, onViewChange }: Props): JSX.Element {
   return (
     <ToggleButtonGroup
       value={view}
       exclusive
-      onChange={(_, newView) => { if (newView) onViewChange?.(newView); }}
+      onChange={(_, newView) => {
+        if (newView) onViewChange?.(newView);
+      }}
       size="small"
       sx={{ mr: 1, height: 40 }}
     >
